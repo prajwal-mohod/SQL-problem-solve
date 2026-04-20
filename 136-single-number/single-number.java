724ae1afbe5a228c0c69fb0574b2ef062a1caee1
Prajwal Mohod <161816466+prajwal-mohod@users.noreply.github.com>
2026-04-21 00:41:22 +0530

    Time: 532 ms (5.26%) | Memory: 47 MB (50.20%) - LeetSync

---
+++ 136-single-number/single-number.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int singleNumber(int[] arr) {
+        
+        for(int i=0; i<arr.length-1; i++){
+
+            boolean isUnique = true;
+            for(int j = 0; j<arr.length; j++){
+
+                if(arr[i] == arr[j] && i!=j){
+                    isUnique = false;
+                    continue;
+                }
+            }
+            if(isUnique){
+                return arr[i];
+            }
+        }
+        return arr[arr.length-1];
+    }
+}
